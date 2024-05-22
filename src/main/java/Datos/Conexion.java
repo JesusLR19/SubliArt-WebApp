885f715957aa47c910b1aa1184024f87b5eb76de
JesusLR19 <jesuseduardolopezro@gmail.com>
2024-05-22 14:40:37 -0600

    conexion a la bd y prueba

---
+++ src/main/java/Datos/Conexion.java
@@ -0,0 +1,50 @@
+package Datos;
+import java.sql.*;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+public class Conexion {
+    private static String user = "postgres";
+    private static String password = "211910"; //colocar la contraseña que tenga en su sgbd
+    private static String bd = "Subli-Art"; //colocar el nombre con el que hicieron su bd con los scripts
+    private static String server = "jdbc:postgresql://localhost:5432/" + bd;
+    private static String driver = "org.postgresql.Driver";
+
+    private static BasicDataSource ds;
+
+    public static DataSource getDataSource(){
+        if(ds == null){
+            ds = new BasicDataSource();
+            ds.setUrl(server);
+            ds.setUsername(user);
+            ds.setPassword(password);
+            ds.setInitialSize(50);
+            ds.setDriverClassName(driver);
+        }
+        return ds;
+    }
+    public static Connection getConnection() throws SQLException{
+        return getDataSource().getConnection();
+    }
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        }catch (SQLException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+    public static void close(PreparedStatement ps){
+        try{
+            ps.close();
+        }catch (SQLException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+    public static void close(Connection conn){
+        try{
+            conn.close();
+        }catch (SQLException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+}
---
+++ src/main/java/Datos/ConexionMain.java
@@ -0,0 +1,27 @@
+package Datos;
+import java.sql.*;
+public class ConexionMain {
+    public static void main(String[]args){
+        try{
+            Connection conn = Conexion.getConnection();
+
+            String sql = "SELECT * FROM usuarios";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                int id_usuario = rs.getInt("id_usuario");
+                String nombre = rs.getString("nombre");
+                String apellido_p = rs.getString("apellido_p");
+                String apellido_m = rs.getString("apellido_m");
+                String username = rs.getString("username");
+                int id_contacto = rs.getInt("id_contacto");
+                int id_rol = rs.getInt("id_rol");
+                boolean estatus = rs.getBoolean("estatus");
+                System.out.println("ID="+ id_usuario + "\nNombre:" + nombre +" Apellidos: " + apellido_p + " " + apellido_m + "\nUsername:" + username + "\nID_Contacto: " + id_contacto
+                + "\nID_Rol: " + id_rol + "\nEstatus: " + estatus);
+            }
+        }catch (SQLException ex){
+            ex.printStackTrace(System.out.printf("Fallo en la conexion"));
+        }
+    }
+}
